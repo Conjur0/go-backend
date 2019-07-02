430b331aa6ff8fa15243fe1182df135b61071353
Yonneh <yonneh@yonneh.ga>
2019-07-02 23:52:51 +0000

    changes

--- go-backend.sh
+++ go-backend.sh
@@ -8,7 +8,7 @@ if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= | grep -P "^\s*$(cat ${PIDFILE
   exit 99
 fi
 
-/home/yonneh/go-backend 1>>$LOGFILE 2>>$ERRFILE &
+/stor/go/src/github.com/Conjur0/go-backend/go-backend 1>>$LOGFILE 2>>$ERRFILE &
 
 echo $! > "${PIDFILE}"
 chmod 644 "${PIDFILE}"
