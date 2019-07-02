fe7e3fd77d06050fa40f247876e8eb3567561877
Yonneh <yonneh@yonneh.ga>
2019-07-02 03:09:57 +0000

    changes

---
+++ go-backend.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+PIDFILE="/stor/go/src/github.com/Conjur0/go-backend/go-backend.pid"
+LOGFILE="/stor/go/src/github.com/Conjur0/go-backend/go-backend.`date +'%Y-%m-%d'`.log"
+ERRFILE="/stor/go/src/github.com/Conjur0/go-backend/go-backend.err"
+
+if [ -e "${PIDFILE}" ] && (ps -u $(whoami) -opid= | grep -P "^\s*$(cat ${PIDFILE})$" &> /dev/null); then
+  echo "Already running."
+  exit 99
+fi
+
+/home/yonneh/go-backend 1>>$LOGFILE 2>>$ERRFILE &
+
+echo $! > "${PIDFILE}"
+chmod 644 "${PIDFILE}"
+
